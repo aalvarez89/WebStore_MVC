39581befdca3d36169711ceb6b1b7dcee46723d8
Emma F <emmaf@Emmas-MacBook-Pro.local>
2020-10-28 19:28:16 -0400

    images not showing up in heroku bc it is case sensitive

--- models/products.js
+++ models/products.js
@@ -3,7 +3,7 @@ const product = {
     initDB() {
         
         this.fakedb.push({
-            img: "i1.png", 
+            img: "i1.PNG", 
             title: "Maple-Mustard Glazed Mini Meatloaves", 
             included: "Meat and Vegetables",
             synopsis: "over Mashed Potatoes and Roasted Broccoli", 
@@ -15,7 +15,7 @@ const product = {
             topPackage: true
         })
         this.fakedb.push({
-            img: "i2.png", 
+            img: "i2.PNG", 
             title: "Cider Braised Chicken and Apples", 
             included: "Meat and Vegetables",
             synopsis: "with Kale and Roasted Potatoes", 
@@ -27,7 +27,7 @@ const product = {
             topPackage: false           
         })
         this.fakedb.push({
-            img: "i3.png", 
+            img: "i3.PNG", 
             title: "Hearty Roasted Root Vegetable Coconut Curry", 
             included: "Vegetables and Curry",
             synopsis: "with Chickpeas and Warm Naan", 
@@ -39,7 +39,7 @@ const product = {
             topPackage: true          
         })
         this.fakedb.push({
-            img: "i4.png", 
+            img: "i4.PNG", 
             title: "Sesame-Cashew Butter Shanghai Noodles", 
             included: "Vegetables and Noodles",
             synopsis: "with Sauteed Vegetables and Celery-Radish Slaw", 
