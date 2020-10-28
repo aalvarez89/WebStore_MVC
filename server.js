e6637a47a18f0674701bed4f355994801f801177
Emma F <emmaf@Emmas-MacBook-Pro.local>
2020-10-28 15:38:33 -0400

    validation in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// email
+// email a2
 const apiKey = require("./models/email"); 
 const emailService = new apiKey(); 
 
@@ -49,6 +49,62 @@ app.get("/register", (req, res)=> {
     })
 }); 
 
+// REGISTRATION -> FORM VALIDATION
+app.post("/register", (req, res)=> {
+    let errors = {
+        m_messages: [], 
+        m_firstName: "",
+        m_lastName: "", 
+        m_email: ""
+    }
+    // FIRST NAME
+    if (req.body.FirstName == "") {
+        errors.m_messages.push("Enter your first name"); 
+    } else {
+        errors.m_firstName = req.body.FirstName; 
+    }
+    // LAST NAME
+    if (req.body.LastName == "") {
+        errors.m_messages.push("Enter your last name"); 
+    } else {
+        errors.m_lastName = req.body.LastName; 
+    }
+    // EMAIL
+    if (req.body.email == "") {
+        errors.m_messages.push("Enter your email"); 
+    } else {
+        // must be anystring@anystring.anystring
+        if (!req.body.email.match(/\S+@\S+\.\S+/)) {
+            errors.m_messages.push("Enter a valid email"); 
+        } else {
+            errors.m_email = req.body.email; 
+        }
+    }
+    // PASSWORD
+    if (req.body.password =="") {
+        errors.m_messages.push("Enter a password"); 
+    } else if (!req.body.password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)) {
+        errors.m_messages.push("Password must have at least 8 characters, a letter, a number, and a special character"); 
+    }
+    
+    if (errors.m_messages.length>0) {
+        res.render("register", errors);
+        console.log(errors.m_messages); 
+    } else {
+        const info = {
+            from: 'Food Delivery Service <mailgun@samples.mailgun.org>',
+            to: req.body.email, 
+            subject: `Welcome ${req.body.FirstName}`, 
+            text: "Thank you for signing up with our delivery service"
+        };
+        mailgun.messages().send(info, (error, body)  => {
+            console.log(body); 
+        });
+        res.redirect('/'); 
+    }
+})
+
+
 // LOGIN
 app.get("/login", (req, res)=> {
     res.render("login", {
@@ -57,8 +113,37 @@ app.get("/login", (req, res)=> {
     })
 }); 
 
-const port = 3000; 
+// LOGIN -> FORM VALIDATION
+app.post("/login", (req, res)=> {
+    let errors = {
+        m_messages: [], 
+        m_email: ""
+    }
+    // EMAIL
+    if (req.body.email == "") {
+        errors.m_messages.push("Enter your email"); 
+    } else {
+        // must be anystring@anystring.anystring
+        if (!req.body.email.match(/\S+@\S+\.\S+/)) {
+            errors.m_messages.push("Enter a valid email"); 
+        } else {
+            errors.m_email = req.body.email; 
+        }
+    }
+    // PASSWORD
+    if (req.body.password =="") {
+        errors.m_messages.push("Enter a password"); 
+        // must be minimum 8 characters, 1+ letter, 1+ number, 1+ special character
+    }
+    
+    if (errors.m_messages.length>0) {
+        res.render("login", errors);
+        console.log(errors.m_messages); 
+    } 
+    res.redirect('/'); 
+})
+const PORT = process.env.PORT || 3000;  
 
-app.listen(port, ()=> {
+app.listen(PORT, ()=> {
     console.log(`The web server is up and running`); 
 })
